d6fc39411429a00eca4ffc09099727cd6df51ca0
Shamil Fattakhov <shama2195@bk.ru>
2022-12-16 21:25:38 +0300

    add Task008 //1 seminar homework

---
+++ HOMEWORK_/1S_Task008/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая на вход принимает число (N),
+//а на выходе показывает все чётные числа от 1 до N.
+Console.Clear();
+Console.Write("Введите число от 1 до 100: ");
+int N = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Все четные числа в интервале от 1 до {N}:");
+int index = 1;
+
+while (index < N) //для примера в интервале (1, N) НЕ включая N
+{
+    if(index%2 == 0)
+    {
+        Console.Write($"{index} ");
+    }
+    index++;
+}
