e484a8ea40ba13a38c099a640676d0b130ed1826
Shamil Fattakhov <shama2195@bk.ru>
2022-12-16 21:23:38 +0300

    add Task004 //1 seminar homework

---
+++ HOMEWORK_/1S_Task004/Program.cs
@@ -0,0 +1,72 @@
+﻿//Напишите программу, которая принимает на вход
+//три числа и выдаёт максимальное из этих чисел.
+Console.Clear();
+Console.Write("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+Console.Write("Введите число C: ");
+int numberC = int.Parse(Console.ReadLine());
+
+int max = numberA;
+
+if(numberA > numberB)
+{
+    if(numberA > numberC)
+    {
+        Console.WriteLine($"MAX = {max}");
+    }
+
+    else
+    {
+        max = numberC;
+        Console.WriteLine($"MAX = {max}");
+    }
+}
+
+else
+{
+    max = numberB;
+    if(numberB > numberC)
+    {
+        Console.WriteLine($"MAX = {max}");
+    }
+
+    else
+    {
+        max = numberC;
+        Console.WriteLine($"MAX = {max}");
+    }
+}
+
+//2 вариант
+Console.Write("Введите число A: ");
+int numberA2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB2 = int.Parse(Console.ReadLine());
+Console.Write("Введите число C: ");
+int numberC2 = int.Parse(Console.ReadLine());
+
+int max2 = numberA2;
+
+if(numberA2 > numberB2)
+{
+    if(numberA2 > numberC2);
+
+    else
+    {
+        max2 = numberC2;
+    }
+}
+
+else
+{
+    max2 = numberB2;
+    if(numberB2 > numberC2);
+
+    else
+    {
+        max2 = numberC2;
+    }
+}
+Console.WriteLine($"MAX = {max2}");
