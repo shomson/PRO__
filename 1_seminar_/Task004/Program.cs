93a72319d07281b4fe876aeac40a902a0649996e
Shamil Fattakhov <shama2195@bk.ru>
2022-12-16 21:16:02 +0300

    add Task004

---
+++ 1_seminar_/Task004/Program.cs
@@ -0,0 +1,12 @@
+﻿//При вводе любого числа необходимо отобразить последнюю цифру.
+
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int N = number%100; //% - (аргумент/оператор/функция) для вывода остатка от деления
+
+int number2 = number / 100;
+
+int N2 = number2%10;
+
+Console.WriteLine($"{N2}");
