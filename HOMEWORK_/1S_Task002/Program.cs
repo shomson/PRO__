3cd908afebd2fe22076f9393dd550267f5c0c2fe
Shamil Fattakhov <shama2195@bk.ru>
2022-12-16 21:22:37 +0300

    add Task002 //1 seminar homework

---
+++ HOMEWORK_/1S_Task002/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите число А: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число Б: ");
+int numberB = int.Parse(Console.ReadLine());
+
+if(numberA == numberB)
+{
+    Console.WriteLine("Числа А и Б - равны.");
+}
+
+else
+{
+    if(numberA < numberB)
+    {
+        Console.WriteLine($"MIN(А) = {numberA}, MAX(Б) = {numberB}");
+    }
+
+    else
+    {
+        Console.WriteLine($"MIN(Б) = {numberB}, MAX(А) = {numberA}");
+    }
+}
