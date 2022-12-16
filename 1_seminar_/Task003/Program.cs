5546abf8c1706c517b5ad4c5f912499a8ef2b463
Shamil Fattakhov <shama2195@bk.ru>
2022-12-16 21:15:13 +0300

    add Task003

---
+++ 1_seminar_/Task003/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая на вход принимает одно число (N),
+//а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.Write("Введите число от 0 до 10: ");
+int N = int.Parse(Console.ReadLine());
+int meanN = -N;
+
+while (meanN <= N)
+{
+    Console.Write($"{meanN} ");
+    meanN++;
+}
