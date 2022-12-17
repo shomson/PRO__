2ba31723090b928eacc18baaea665b1758a0fbd5
Shamil Fattakhov <shama2195@bk.ru>
2022-12-17 16:30:49 +0300

    add Ex002_MethodArray + Ex001 changes

---
+++ 2_lection_/Ex002_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 34, 56, 23, 67, 221, 568, 12, 23};
+
+int n = array.Length; //Length - содержит информацию о длине массива (кол-ве чисел)
+int find = 23;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break; //останавливает цикл при выполнении условия (при повторяющихся значениях чисел в массиве)
+    }
+    index++; //index = index + 1;
+}
