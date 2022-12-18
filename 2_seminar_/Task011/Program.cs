2056b5cec3da92a13e815556c8a238ee818d6625
Shamil Fattakhov <shama2195@bk.ru>
2022-12-18 14:19:38 +0300

    add Task011

---
+++ 2_seminar_/Task011/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая выводит случайное 
+//трехзначное число и удаляет вторую цифру этого числа.
+Console.Clear();
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Число {number}.");
+
+int middleSniper(int number)
+{
+    int firstDecade = number %10;
+    int thirdDecade = number / 100;
+    int result = thirdDecade * 10 + firstDecade;
+    return result;
+}
+
+int split = middleSniper(number);
+Console.WriteLine($"Новое число {split}.");
+
+//группа сделала хитрее: Console.WriteLine($"{number} -> {thirdDecade}{firstDecade}");
