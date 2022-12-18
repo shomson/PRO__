862ac848d11a05916a49b12e43a2cf83bc8d2df1
Shamil Fattakhov <shama2195@bk.ru>
2022-12-18 15:43:56 +0300

    add Task016 //квадраты чисел

---
+++ 2_seminar_/Task016/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет, 
+//является ли одно число квадратом другого.
+Console.Clear();
+
+Console.Write("Введите число А: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число Б: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{numberA} / {numberB}");
+
+int sqrA = Convert.ToInt32(Math.Pow(numberA, 2));
+int sqrB = Convert.ToInt32(Math.Pow(numberB, 2));
+
+if((sqrA == numberB) || (sqrB == numberA))
+{
+    Console.WriteLine("Одно из чисел квадрат другого.");
+}
+else
+{
+    Console.WriteLine("Ни одно из чисел НЕ является квадратом другого.");
+}
