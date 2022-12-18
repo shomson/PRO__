1bf6ca1bc1786b7f6c653f4cf4a0ad335363b0c7
Shamil Fattakhov <shama2195@bk.ru>
2022-12-18 13:34:51 +0300

    add Ex000 //пример вначале семинара

---
+++ 2_seminar_/Ex000/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Число {number}.");
+int firstDecade = number %10;
+int secondDecade = number / 10;
+if(secondDecade == firstDecade)
+{
+    Console.WriteLine($"Числа {secondDecade} и {firstDecade} равны.");
+}
+else
+{
+    if(secondDecade > firstDecade)
+    {
+        Console.WriteLine($"Число {secondDecade} больше {firstDecade}.");
+    }
+    else
+    {
+        Console.WriteLine($"Число {firstDecade} больше {secondDecade}.");
+    }
+}
