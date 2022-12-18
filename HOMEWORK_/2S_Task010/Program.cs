b7cebfbaece041fbf1e249ca8ba9fa617ad20c57
Shamil Fattakhov <shama2195@bk.ru>
2022-12-18 19:01:28 +0300

    add Task010 //2 seminar homework

---
+++ HOMEWORK_/2S_Task010/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+int number = new Random().Next(100, 1000);
+Console.Write($"{number} -> ");
+
+int middleNumber(int number) //создание метода для тренировки
+{
+    int secondDecade = number %100;
+    int result = secondDecade / 10;
+    return result;
+}
+
+int middleNum = middleNumber(number);
+Console.WriteLine($"cредняя цифра  {middleNum}.");
