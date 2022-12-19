8fe2f122f97eea9027a0f300a02688473250fffa
Shamil Fattakhov <shama2195@bk.ru>
2022-12-19 11:45:01 +0300

    add Task015 // 2 seminar homework

---
+++ HOMEWORK_/2S_Task015/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Clear();
+
+void weekDayOrEnd(int weekDayNum)
+{
+    while(weekDayNum <1 || 7< weekDayNum)
+       {
+           Console.Write("Некорректная цифра! Попробуйте снова: ");
+           weekDayNum = int.Parse(Console.ReadLine());
+       }
+    
+    if(weekDayNum == 6 || weekDayNum == 7)
+       {
+           Console.Write($"{weekDayNum}-й это выходной.");
+       }
+    else
+       {
+           Console.Write($"{weekDayNum}-й это будний день.");
+       }
+}
+
+Console.Write("Введите цифру дня недели: ");
+int weekDayNum = int.Parse(Console.ReadLine());
+weekDayOrEnd(weekDayNum);
