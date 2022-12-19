f771b5908069bed62e71523b29683e2433691ece
Shamil Fattakhov <shama2195@bk.ru>
2022-12-19 10:28:24 +0300

    add changes to Task013

--- HOMEWORK_/2S_Task013/Program.cs
+++ HOMEWORK_/2S_Task013/Program.cs
@@ -2,23 +2,21 @@
 //что третьей цифры нет.
 Console.Clear();
 
-//int number = new Random().Next(1, 100000);
-int number = 123;
+int number = new Random().Next(1, 100000);
+//int number = 123;
 Console.WriteLine($"Число {number}.");
 
-int index = 1;
-int count = -1;
+int index = 0;
 
-while(number != number %index)
+while(number != number %Convert.ToInt32(Math.Pow(10, index)))
 {
-    index = index * 10;
-    count++;
-}
-
-Console.WriteLine($"{index} / {count}");
-if(count > 1)
+    index++;
+} //после выхода из цикла index показывает разрядность числа
+ 
+//Console.WriteLine($"index {index}");
+if(index >= 3)
 {
-    int NUM = number / Convert.ToInt32(Math.Pow(10, count - 2));
+    int NUM = number / Convert.ToInt32(Math.Pow(10, index - 3)); //в параметре [-3] задается цифра которая вам нужна
     int remainder = NUM %10;
     Console.WriteLine($"Его третья цифра -> {remainder}.");
 }
