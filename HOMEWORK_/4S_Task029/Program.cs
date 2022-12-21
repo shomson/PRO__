8a331a8dce016c3ff64fe97847b892b89d68ce91
Shamil Fattakhov <shama2195@bk.ru>
2022-12-21 20:08:43 +0300

    add Task029

---
+++ HOMEWORK_/4S_Task029/Program.cs
@@ -0,0 +1,31 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+Console.Clear();
+
+// int[] array = GetBinaryArray(8);
+// Console.WriteLine($"[{String.Join(" | ", array)}]"); 
+
+// int[] GetBinaryArray(int size)
+// {
+//     int[] array = new int[size];
+
+//     for(int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(0, 100);
+//     }
+//     return array;
+// }
+
+void ArrayRandom(int size)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(0, 100);
+        Console.Write($"{array[i]} ");
+    }
+}
+
+Console.Write("Напишите кол-во элементов в массиве: ");
+int length = int.Parse(Console.ReadLine());
+ArrayRandom(length);
