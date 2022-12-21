cdc859c18512fedca556b3b6337710e3c230591b
Shamil Fattakhov <shama2195@bk.ru>
2022-12-21 19:55:48 +0300

    add Task027

---
+++ HOMEWORK_/4S_Task027/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+
+Console.Write("Введите положительное число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write($"Сумма цифр числа {number} равна {GetNumsSum(number)}.");
+
+int GetNumsSum(int number)
+{
+    int sum = 0;
+    int remainder = 0;
+    while(number > 0)
+        {
+            remainder = number %10;
+            number = number / 10;
+            sum = sum + remainder;
+        }
+    return sum;
+}
+
