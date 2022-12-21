6dd8b613038f28dfbb69f5ea9b04c6e329bc8a4d
Shamil Fattakhov <shama2195@bk.ru>
2022-12-21 23:52:21 +0300

    add changes to Task025

--- HOMEWORK_/4S_Task025/Program.cs
+++ HOMEWORK_/4S_Task025/Program.cs
@@ -2,34 +2,48 @@
 //и возводит число A в натуральную степень B.
 Console.Clear();
 
-// void DegreeA()
-// {
-//     Console.Write("Введите число A: ");
-//     int A = int.Parse(Console.ReadLine());
-//     Console.Write("Введите число B: ");
-//     int B = int.Parse(Console.ReadLine());
+void DegreeA()
+{
+    Console.Write("Введите число A: ");
+    double A = int.Parse(Console.ReadLine());
+    Console.Write("Введите число B: ");
+    int B = int.Parse(Console.ReadLine());
     
-//     double DegreeA = A;
-//     for (int i = 1; i < B; i++)
-//     {
-//         DegreeA = DegreeA * A;
-//     }
-//     Console.WriteLine($"Число {A} в {B} равно {DegreeA}.");
-// }
-
-// DegreeA(); //не работает для B < 0, = 0 / A = 0 && B = 0
-
-Console.Write("Введите число >1: ");
-int N = int.Parse(Console.ReadLine());
-
-int i = 1;
-if(N < 1)
+    if(B == 0) Console.WriteLine($"Число {A} в степени {B} равно 1.");
+    
+    if(B > 0)
     {
-        i = N;
-        N = 1;
+        if(A != 0)
+        {
+            double DegreeA = A;
+
+            for (int i = 1; i < B; i++)
+            {
+                DegreeA = DegreeA * A;
+            }
+            Console.WriteLine($"Число {A} в степени {B} равно {DegreeA}.");
+        }
+        else Console.WriteLine($"Число {A} в степени {B} равно 0.");
     }
 
-for( ; i <= N; i++)
-    {
-        Console.WriteLine($"{i} -> {Math.Pow(i, 3)}");
+    if(B < 0)
+    {   
+        if(A == 0) Console.WriteLine($"Число {A} в степени {B} равно ∞.");
+        if(A == 1) Console.WriteLine($"Число {A} в степени {B} равно 1.");
+        if(A != 0 && A != 1)
+        {
+            Console.Write($"Число {A} в степени {B} равно ");
+            A = (1 / A);
+            B = B * -1;
+            double DegreeA = A;
+
+            for (int i = 1; i < B; i++)
+            {
+                DegreeA = DegreeA * A;
+            }
+            Console.WriteLine($"{Math.Round(DegreeA, 5)}.");
+        }
     }
+}
+
+DegreeA();
