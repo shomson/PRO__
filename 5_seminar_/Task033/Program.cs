3c10590c9cb6227828349c541740c8b493002439
Shamil Fattakhov <shama2195@bk.ru>
2023-01-10 20:03:26 +0300

    add Task033 //поиск числа в массиве

---
+++ 5_seminar_/Task033/Program.cs
@@ -0,0 +1,31 @@
+﻿//Задайте массив. Напишите программу, которая определяет, 
+//присутствует ли заданное число в массиве.
+Console.Clear();
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+bool FindNum(int num, int[] array)
+{
+    foreach(int el in array)
+    {
+        if(num == el)
+        return true;
+    }
+    return false;
+}
+
+int[] array = GetRandomArray(10, 1, 10);
+int num = 5;
+
+Console.WriteLine($"[{String.Join(" | ", array)}]");
+if(FindNum(num, array)) Console.WriteLine($"Число {num} - найдено.");
+else Console.WriteLine($"Число {num} - НЕ найдено.");
