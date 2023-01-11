330de8cd2532d1369cbcc13c92690311cfbef129
Shamil Fattakhov <shama2195@bk.ru>
2023-01-11 17:22:06 +0300

    add Task036

---
+++ HOMEWORK_/5S_Task036/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+Console.Clear();
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int SumNotEvenPositions(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i = i + 2)
+    {
+        sum += array[i];
+    }
+    return sum;
+}
+
+int[] array = GetRandomArray(10, -10, 10);
+Console.WriteLine($"[{String.Join(" | ", array)}]");
+Console.WriteLine($"Сумма элементов на нечетных позициях равна {SumNotEvenPositions(array)}.");
