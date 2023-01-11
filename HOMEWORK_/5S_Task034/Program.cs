2de7065466c42c7c44569bcae59de56f2e6edba8
Shamil Fattakhov <shama2195@bk.ru>
2023-01-11 17:04:45 +0300

    add Task034

---
+++ HOMEWORK_/5S_Task034/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+Console.Clear();
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int FindPosNums(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i]% 2 == 0) count += 1;
+    }
+    return count;
+}
+
+int[] array = GetRandomArray(10, 100, 1000);
+Console.WriteLine($"[{String.Join(" | ", array)}]");
+Console.WriteLine($"Количество чёных чисел в массиве - {FindPosNums(array)}.");
