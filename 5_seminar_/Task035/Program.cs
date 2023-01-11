09a63440e1d3f514e4dadb19f496f035119da1cb
Shamil Fattakhov <shama2195@bk.ru>
2023-01-11 16:40:36 +0300

    add Task037 + add changes to Task035

--- 5_seminar_/Task035/Program.cs
+++ 5_seminar_/Task035/Program.cs
@@ -1,24 +1,30 @@
-﻿//Задайте одномерный массив из 123 случайных чисел. 
-//Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+﻿//Задайте одномерный массив из 123 случайных чисел.
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
 Console.Clear();
 
 int[] GetRandomArray(int size, int minValue, int maxValue)
 {
     int[] array = new int[size];
 
-    for(int i = 0; i < size; i++)
+    for (int i = 0; i < size; i++)
     {
         array[i] = new Random().Next(minValue, maxValue + 1);
     }
     return array;
 }
 
+int FindNums(int[] array, int min, int max)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (min <= array[i] && array[i] <= max)
+            count += 1;
+    }
+    return count;
+}
+
 int[] array = GetRandomArray(10, -100, 100);
 Console.WriteLine($"[{String.Join(" | ", array)}]");
-
-int count = 0;
-for (int i = 0; i < array.Length; i++)
-{
-    if(10 <= array[i] && array[i] <= 99) count += 1;
-}
+int count = FindNums(array, 10, 99);
 Console.WriteLine($"Количество элементов в отрезке [10, 99] - {count}.");
---
+++ 5_seminar_/Task037/Program.cs
@@ -0,0 +1,32 @@
+﻿//Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, 
+//второй и предпоследний и т.д. Результат запишите в новом массиве.
+Console.Clear();
+
+int[] GetRandomArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int[] NumsComposition(int[] array)
+{
+    int[] result = new int[(array.Length + 1) / 2];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length - i - 1];
+        Console.WriteLine($"{result[i]} = {array[i]} * {array[array.Length - i - 1]}");
+    }
+    if(array.Length % 2 == 1) result[result.Length - 1] = array[array.Length / 2];
+    return result;
+}
+
+int[] array = GetRandomArray(15, -20, 20);
+Console.WriteLine($"[{String.Join(" | ", array)}]");
+Console.WriteLine($"Произведения пар в массиве:");
+int[] result = NumsComposition(array);
+Console.Write($"[{String.Join(" | ", result)}]");
