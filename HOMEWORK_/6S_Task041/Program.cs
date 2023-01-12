c34a63341abac6daada7e8b68e270d61cd5bcd65
Shamil Fattakhov <shama2195@bk.ru>
2023-01-12 15:52:18 +0300

    add Task041 //перевод чисел из строки в массив

---
+++ HOMEWORK_/6S_Task041/Program.cs
@@ -0,0 +1,32 @@
+﻿//Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Clear();
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int[nums.Length];
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        result[i] = int.Parse(nums[i]);
+    }
+    return result;
+}
+
+int FindPositiveNums(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        count += 1;
+    }
+    return count;
+}
+
+Console.Write("Введите числа через пробел: ");
+string numbers = Console.ReadLine();
+int[] array = GetArrayFromString(numbers);
+// Console.WriteLine(String.Join(" ", array));
+Console.Write($"Кол-во чисел больше нуля - {FindPositiveNums(array)}.");
