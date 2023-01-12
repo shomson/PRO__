cee8152ddc5e774af1306461367afc9be18d2158
Shamil Fattakhov <shama2195@bk.ru>
2023-01-12 15:12:44 +0300

    add Task044 //Фибоначчи без рекурсии

---
+++ 6_seminar_/Task044/Program.cs
@@ -0,0 +1,26 @@
+﻿//Не используя рекурсию, выведите первые N чисел Фибоначчи.
+Console.Clear();
+
+int[] Fibonacci(int N)
+{
+    int[] result = new int[N];
+    for (int i = 0; i < N; i++)
+    {
+        result[i] = i;
+        if (i == 1) break;
+    }
+
+    if (N > 2)
+        {
+            for (int j = 2; j < N; j++)
+            {
+             result[j] = result[j - 2] + result[j - 1];  
+            }
+        }
+    return result;
+}
+
+Console.WriteLine("Сколько чисел Фибоначчи вы хотите увидеть?");
+int number = int.Parse(Console.ReadLine());
+int[] fibonacciArray = Fibonacci(number);
+Console.WriteLine($"[{String.Join("_", fibonacciArray)}]");
