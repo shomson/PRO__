a18b3ae25dd0d85f366b6d869704a56565c578ad
Shamil Fattakhov <shama2195@bk.ru>
2023-01-18 14:47:43 +0300

    add changes to Task043 //решение преподаваеля

--- HOMEWORK_/6S_Task043/Program.cs
+++ HOMEWORK_/6S_Task043/Program.cs
@@ -24,3 +24,26 @@ Console.Write("Введите k2: ");
 int k2 = int.Parse(Console.ReadLine());
 
 Console.Write($"Точка пересечения прямых -> ({IntersectionCoor(b1, k1, b2, k2)})");
+
+//Решение преподавателя
+
+// Clear();
+// Write("Введите через пробел b1, k1, b2 , k2: ");
+// string[] f = ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+// double b1 = double.Parse(f[0]);
+// double k1 = double.Parse(f[1]);
+// double b2 = double.Parse(f[2]);
+// double k2 = double.Parse(f[3]);
+
+// WriteLine(String.Join(" ", GetPoint(b1, k1, b2, k2)));
+
+
+
+
+// double[] GetPoint(double inB1, double inK1, double inB2, double inK2)
+// {
+//     double[] result = new double[2];
+//     result[0] = (inB2 - inB1) / (inK1 - inK2);
+//     result[1] = inK1 * result[0] + inB1;
+//     return result;
+// }
