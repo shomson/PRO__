2a98beb6788d5ae56fd94a2921a11d999bcc1a04
Shamil Fattakhov <shama2195@bk.ru>
2023-01-20 15:50:47 +0300

    add Task052 //среднее арифметическое столбца

---
+++ HOMEWORK_/7S_Task052/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задайте двумерный массив из целых чисел. 
+//Найдите среднее арифметическое элементов в каждом столбце.
+using System;
+using static System.Console;
+Clear();
+
+int[,] GetMatrix(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j]} | ");
+        }
+        
+    WriteLine();
+    }
+}
+
+double[] ColumnAverageSum(int[,] matrix)
+{
+    double[] result = new double[matrix.GetLength(1)];
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double sum = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i, j];
+        }
+        result[j] = sum / matrix.GetLength(0);
+    }
+    return result;
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] matrix = GetMatrix(rows, columns, 0, 9);
+PrintMatrix(matrix);
+WriteLine();
+Write($"{String.Join(" | ", ColumnAverageSum(matrix))} <- cреднее арифметическое каждого столбца.");
