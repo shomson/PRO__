ba838a6966bef8bdef8a5b4623d7441533fef719
Shamil Fattakhov <shama2195@bk.ru>
2023-01-20 16:31:01 +0300

    add Task053 //замена строк матрицы

---
+++ 8_seminar_/Task053/Program.cs
@@ -0,0 +1,55 @@
+﻿//Задайте двумерный массив. 
+//Напишите программу, которая поменяет местами первую и последнюю строку массива.
+using System;
+using static System.Console;
+Clear();
+
+int[,] GetMatrix(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < result.GetLength(0); i++)
+    {
+        for (int j = 0; j < result.GetLength(1); j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Write($"{matrix[i, j]} ");
+        }
+        
+    WriteLine();
+    }
+}
+
+void ReplaceRows(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            int buffer = matrix[i, j];
+            matrix[i, j] = matrix[matrix.GetLength(0) - i - 1, j];
+            matrix[matrix.GetLength(0) - i - 1, j] = buffer;
+        }
+    }
+}
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine());
+
+int[,] matrix = GetMatrix(rows, columns, 0, 9);
+PrintMatrix(matrix);
+WriteLine();
+ReplaceRows(matrix);
+PrintMatrix(matrix);
