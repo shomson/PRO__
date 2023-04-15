eb2b2d33b3d5d019b88babbfef772b76b7bb2374
Shamil Fattakhov <shama2195@bk.ru>
2023-04-15 21:59:58 +0300

    add Task063 //простая рекурсия

---
+++ 9_seminar/Task063/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте значение N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от 1 до N.
+using System;
+using static System.Console;
+
+Clear();
+
+// void NumbersRow(int num)
+// {
+//     if (num >= 1)
+//     {
+//         Write($"{num} ");
+//         NumbersRow(num - 1);
+//     }
+// }
+// 
+// NumbersRow(7);
+
+string PrintNumbers(int endNum)
+{
+    if (endNum == 1) return "1";
+    return (PrintNumbers(endNum - 1) + ' ' + endNum.ToString());
+}
+
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+WriteLine(PrintNumbers(n));
