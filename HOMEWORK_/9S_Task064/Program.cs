07b7249ecffa85a1bcf1282c6129db33e12549ac
Shamil Fattakhov <shama2195@bk.ru>
2023-04-15 23:59:08 +0300

    add Task064 //числа в промежутке

---
+++ HOMEWORK_/9S_Task064/Program.cs
@@ -0,0 +1,27 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая выведет все натуральные числа в промежутке от N до M.
+using System;
+using static System.Console;
+
+Clear();
+
+string PrintNumbers(int startNum, int endNum)
+{
+    if (startNum < endNum)
+    {
+        return (endNum.ToString() + ' ' + PrintNumbers(startNum, endNum - 1));
+    }
+    else
+    {
+        if (endNum == startNum) return $"{startNum}";
+        return (PrintNumbers(startNum - 1, endNum) + ' ' + startNum.ToString());
+    }
+    
+}
+
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+WriteLine(PrintNumbers(m, n));
