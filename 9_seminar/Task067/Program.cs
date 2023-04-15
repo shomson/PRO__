2ebdb3d790040da432de412a76ff8ef59823d83a
Shamil Fattakhov <shama2195@bk.ru>
2023-04-15 23:05:39 +0300

    add Task067 //сумма цифр числа

---
+++ 9_seminar/Task067/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+using System;
+using static System.Console;
+
+Clear();
+
+int PrintSum(int num)
+{
+    if (num == 0)
+    {
+        WriteLine($"0 ");
+        return 0;
+    }
+    int sum = num % 10 + PrintSum(num / 10);
+    WriteLine($"{sum} ");
+    return sum;
+}
+
+Write("Введите число: ");
+int num = int.Parse(ReadLine());
+
+WriteLine($"{num} -> {PrintSum(num)}");
