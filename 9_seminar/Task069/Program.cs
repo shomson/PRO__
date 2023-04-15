d5712960af1cbac3bda7ea97fa55689a11d7dae2
Shamil Fattakhov <shama2195@bk.ru>
2023-04-15 23:19:13 +0300

    add Task069 //степень числа

---
+++ 9_seminar/Task069/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает два числа A и B, 
+//и возводит число А в целую степень B с помощью рекурсии.
+using System;
+using static System.Console;
+
+Clear();
+
+int NumberDegree(int num, int deg)
+{
+    if (deg == 0) return 1;
+    return (num * NumberDegree (num, deg - 1));
+}
+
+Write("Введите A: ");
+int a = int.Parse(ReadLine());
+Write("Введите B: ");
+int b = int.Parse(ReadLine());
+
+WriteLine(NumberDegree(a, b));
