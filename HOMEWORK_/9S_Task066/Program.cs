55bd57955353f5c2a4d7e83b9a9d06900391f090
Shamil Fattakhov <shama2195@bk.ru>
2023-04-16 00:00:31 +0300

    add Task066 //сумма чисел в промежутке

---
+++ HOMEWORK_/9S_Task066/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задайте значения M и N. Напишите программу, 
+//которая найдёт сумму натуральных элементов в промежутке от M до N.
+using System;
+using static System.Console;
+
+Clear();
+
+int SumNumbers(int startNum, int endNum)
+{
+    if (endNum == startNum) return startNum;
+    int sum = SumNumbers(startNum, endNum - 1) + endNum;
+    return sum;
+}
+
+Write("Введите M: ");
+int m = int.Parse(ReadLine());
+Write("Введите N: ");
+int n = int.Parse(ReadLine());
+
+WriteLine(SumNumbers(m, n));
